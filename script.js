cefacecd151ff207c55af90296029fd7a0a39b8a
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-10-12 21:10:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
-
-function firstNonRepeatedChar(s) {
-    // Create a HashMap to store character counts
-    const charCountMap = new Map();
-
-    // Iterate through the string to count character occurrences
-    for (const char of s) {
-        if (charCountMap.has(char)) {
-            charCountMap.set(char, charCountMap.get(char) + 1);
-        } else {
-            charCountMap.set(char, 1);
-        }
-    }
-
-    // Iterate through the string to find the first non-repeated character
-    for (const char of s) {
-        if (charCountMap.get(char) === 1) {
-            return char;
-        }
-    }
+function charCount(s){
+	for(i=0; i<s.length; i++)
+	  let Char = s[i];	
+	{
+		if(s.indexOf(Char) === s.lastIndexOf(Char))	{
+			return Char;
+		}
+		}
+	return null;
+}
